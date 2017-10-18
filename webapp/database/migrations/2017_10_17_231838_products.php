574cd65f59109973d9d92535b6d285fbe98e56b7
Oliver Pardo Maydana <oliverdj87@gmail.com>
2017-10-17 23:46:44 -0400

    Creation Migration, Controller and Model

---
+++ webapp/database/migrations/2017_10_17_231838_products.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Products extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('product_id');
+            $table->string('product_name')->unique();
+            $table->string('product_description');
+            $table->double('product_price');
+            $table->date('product_date_up');
+            $table->date('product_date_down');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
