a784701ae4601bace572abb4f4d559badeb7c275
oscarsegales <oskarsosch>
2017-11-08 22:18:43 -0400

    restableciendo codigo borrado

---
+++ webapp/database/seeds/CitiesTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CitiesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $city = Array("La Paz", "Cochabamba", "Santa Cruz", "Tarija");
+        $c = 1;
+        for ($i=0;$i<4;$i++)
+        {
+
+        DB::table('cities')->insert([
+            'id' => $c++,
+            'name' => $city[$i]]);
+        }
+    }
+}
