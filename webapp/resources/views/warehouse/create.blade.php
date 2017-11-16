6439e39f3fc4a7c157fa9f0ac1c04b58cad8112e
oscarsegales <oskarsosch>
2017-11-16 01:58:32 -0400

    Warehouse ready for tests

--- webapp/resources/views/warehouse/create.blade.php
+++ webapp/resources/views/warehouse/create.blade.php
@@ -1,98 +1,37 @@
 @extends('layouts.app')
 
-@section('menu_warehouse', 'open active')
-@section('title', 'Administracion de Almacenes')
-@section('title-description', 'Administración de Almacenes')
-
 @section('content')
-    <div class="container">
-            <ul class="nav nav-tabs nav-justified" role="tablist" >
-                <li role="presentation" class="active"><a href="#listWh" aria-controls="listWh" role="tab" data-toggle="tab">Lista Almacenes </a></li>
-                <li role="presentation"><a href="#creWh" aria-controls="creWh" role="tab" data-toggle="tab"> Registro Almacenes </a></li>
-                <li role="presentation"><a href="#modWh" aria-controls="modWh" role="tab" data-toggle="tab"> Modificacion Almacenes </a></li>
-                <li role="presentation"><a href="#delWh" onclick="" aria-controls="delWh" role="tab" data-toggle="tab"> Inhabilitacion Almacenes</a></li>
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>REGISTAR ALMACEN</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('warehouse.index') }}"> Volver</a>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            {!! Form::open(array('route' => 'warehouse.store','method'=>'POST','autocomplete'=>'off')) !!}
+            @include('warehouse.form')
+            {!! Form::close() !!}
+        </div>
+        <div class="col-md-6">
+    @if (count($errors) > 0)
+
+        <div class="alert alert-danger">
+            <strong>Por Favor!</strong> Llenar todos los campos requeridos (*)
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
             </ul>
-            <div class="tab-content">
-                <div id="listWh" class="tab-pane fade in active"><br>
-                        <div class="panel-group">
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-md-offset-3 col-md-6 ">
-                                        <div class="panel panel-primary">
-                                            <div class="panel-body">
+        </div>
 
-                                                @if (count($errors)>0)
-                                                    <div class="alert alert-danger">
-                                                        <ul>
-                                                            @foreach ($errors->all() as $error)
-                                                                <li>{{$error}}</li>
-                                                            @endforeach
-                                                        </ul>
-                                                    </div>
-                                                @endif
-
-                                                {{ Form::open(array('Route'=>'store','method'=>'POST','autocomplete'=>'off'))}}
-                                                <label for="">Nombre almacen</label>
-                                                <input type="text" id="" class="form-control" name="name" placeholder="Nombre almacen">
-                                                <label for="">Volumen almacen</label>
-                                                <input type="text" id="" class="form-control" name="volumen" placeholder="Volumen">
-
-                                                <label for="">Sucursal</label>
-                                                <input type="text" id="" class="form-control" name="branches" placeholder="Sucursal">
-
-                                                <label for="" class="">Ciudad</label>
-                                                <select id="" class="form-control" name="city">
-                                                    <option value="">Seleccione una ciudad</option>
-                                                    <option value="1">La Paz</option>
-                                                    <option value="2">Cochabamba</option>
-                                                    <option value="3">Santa Cruz</option>
-                                                    <option value="4">Tarija</option>
-                                                </select>
-
-                                                <label for="">Direccion</label>
-                                                <input type="text" id="" class="form-control" name="address" placeholder="Direccion">
-
-                                                <label for="">Carnet responsable</label>
-                                                <input type="text" id="" class="form-control" name="ci" placeholder="Direccion">
-                                                <br>
-                                                <div class="form-group">
-                                                    <div class="col-sm-11 col-sm-offset-1" align="center">
-                                                        <button type="submit" class="btn btn-success">Registrar</button>
-                                                        <button type="submit" class="btn btn-danger">Cancelar</button>
-                                                    </div>
-                                                </div>
-
-                                                {{Form::close()}}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                </div>
-            </div>
-            <div class="tab-pane fade" id="creWh"><br>
-                <div class="container">
-                    <div class="panel-group">
-
-                    </div>
-                </div>
-            </div>
-            <div class="tab-pane fade" id="modWh"><br>
-                <div class="container">
-                    <div class="panel-group">
-
-                    </div>
-                </div>
-            </div>
-            <div class="tab-pane fade" id="delWh"><br>
-                <div class="container">
-                    <div class="panel-group">
-
-                    </div>
-                </div>
-            </div>
+    @endif
+        </div>
 
     </div>
-@endsection
 
+@endsection
