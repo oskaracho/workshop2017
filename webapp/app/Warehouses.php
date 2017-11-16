4123b4264471522c297f283a7cceaa9ed82a57e1
oscarsegales <oskarsosch>
2017-11-16 01:54:07 -0400

    Cities model

--- webapp/app/Warehouses.php
+++ webapp/app/Warehouses.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Warehouses extends Model
 {
-    //
+
+    protected $fillable = [
+        'name', 'volumen','branches','city','address','user'
+    ];
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
+
+    public function cities(){
+        return $this->hasMany(City::class);
+    }
+
 }
